482c527e240343341cade9f601cbace361608643
Nastassia Martin <nastassia.martin@googlemail.com>
2025-09-07 14:16:29 +0200

    Add geocoding service and location result type

---
+++ src/services/geocoding.ts
@@ -0,0 +1,32 @@
+/**
+ * Open Meteo Geocoding API
+ * https://open-meteo.com/en/docs/geocoding-api>
+ * used to extract longitude and latitude
+ */
+
+import { LocationResult } from "./geocoding.types";
+
+const GEOCODINGURL = "https://geocoding-api.open-meteo.com/v1/search";
+
+/**
+ * Params
+ * @param place place or city
+ * @param count max number of results
+ * @param language language code, english if not available
+ * */
+
+export const getLocation = async (place: string) => {
+	const response = await fetch(
+		`${GEOCODINGURL}/?name=${place}&count=10&language=sv&format=json`
+	);
+
+	if (!response.ok) {
+		throw new Error(`${response.status} ${response.statusText}`);
+	}
+
+	const { results: data }: { results: LocationResult[] } =
+		await response.json();
+
+	// Return location
+	return data;
+};
---
+++ src/services/geocoding.types.ts
@@ -0,0 +1,10 @@
+export interface LocationResult {
+	id: number;
+	name: string;
+	latitude: number;
+	longitude: number;
+	country_code: string;
+	timezone: string;
+	country: string;
+	admin1: string; // Skåne län
+}
